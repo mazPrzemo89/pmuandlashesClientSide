61fc75fbb057736947668cd1c94679b9efbd00f6
mazPrzemo89 <maz89prz@gmail.com>
2020-10-31 11:52:05 +0000

    styling fixes

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -7,47 +7,48 @@ import location from './location.png'
 
 const Footer = () => {
 
-    const history = useHistory() 
+    const history = useHistory()
 
     function showPopup(url) {
-        window.open(url,'name','height=190,width=520,top=200,left=300,resizable');
-     
+        window.open(url, 'name', 'height=190,width=520,top=200,left=300,resizable');
+
     }
 
     let heigth = document.getElementById('root').offsetHeight
-    
-    useEffect(()=>{
-        if(history.location.pathname === '/promotions'){
-            document.getElementById('footer').style.bottom = '0'
-        } 
-    },[])
 
-   
-   
-    
-    return(
-        <div id={'footer'} className={styles.footer} style={{position: window.innerHeight === heigth ? "absolute" : ''}}>
+    useEffect(() => {
+        if (history.location.pathname === '/promotions') {
+            document.getElementById('footer').style.bottom = '0'
+        }
+    }, [])
+
+
+
+
+    return (
+        <div id={'footer'} className={styles.footer} style={{ position: window.innerHeight === heigth ? "absolute" : '' }}>
             <div className={styles.contactDiv}>
                 <div className={styles.contact}>
-                    <img src={phone} className={styles.icon}/>
-                    <p className={styles.contactP}>  +44 7428118398</p>
+                    <img src={phone} className={styles.icon} />
+                    <p className={styles.contactP}>  your phone number</p>
                 </div>
                 <div className={styles.contact}>
-                <img src={mail} className={styles.icon}/>
-                    <p className={styles.contactP}> dagramuda@gmail.com</p>
+                    <img src={mail} className={styles.icon} />
+                    <p className={styles.contactP}> Your email</p>
                 </div>
                 <div className={styles.contact}>
-                <img src={location} className={styles.icon}/>
-                    <p className={styles.contactP}> Gomshall Gardens, CR8 5AH </p>
+                    <img src={location} className={styles.icon} />
+                    <p className={styles.contactP}> Your Adress </p>
                 </div>
             </div>
             <div className={styles.shareButtons}>
-                <a onClick={()=>{showPopup('https://www.facebook.com/sharer/sharer.php?u=https://pmuandlashes.studio')}} className={styles.shareFB}>
-                 </a>
-                <a href='https://www.instagram.com/pmu_and_lash_studio/' target='_blank' className={styles.shareInsta}></a>
-                </div>
+                <a onClick={() => { showPopup('https://www.facebook.com/') }} className={styles.shareFB}>
+                </a>
+                <a href='https://www.instagram.com//' target='_blank' className={styles.shareInsta}></a>
+            </div>
         </div>
-    )}
+    )
+}
 
 
 export default Footer
